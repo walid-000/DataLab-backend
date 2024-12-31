590bbc11a6146d2aae27f47946f016308d943885
walid-000 <walid.shaikh.3810461@ves.ac.in>
2024-12-31 10:52:25 +0000

    setting up server

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/train")
+def train():
+    return {"data " : "walid"}
